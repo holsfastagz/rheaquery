755e79f0b9f532bacf5402a67f06500a3967aecd
HOLSEN MOORE <hols3nmoore@gmail.com>
2025-04-11 21:19:44 -0500

    Fix install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ mkdir $HOME/.local/bin/rheaquery
 cp -r src $HOME/.local/bin/rheaquery
 cp LICENSE $HOME/.local/bin/rheaquery
 
-echo 'export PATH="$HOME/.local/bin/rheaquery:$PATH"' >> $HOME/.bashrc
+echo 'export PATH="$HOME/.local/bin/rheaquery/src/rheaquery:$PATH"' >> $HOME/.bashrc
 
 echo "Rheaquery downloaded successfully. Please run `source ~/.bashrc` for full installation."
