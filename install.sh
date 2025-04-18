39db31beb735c1cc807417197839e23a43249b99
HOLSEN MOORE <hols3nmoore@gmail.com>
2025-04-18 01:29:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 [ -d "$HOME/.local/bin" ] || mkdir $HOME/.local/bin
-mkdir $HOME/.local/bin/rheaquery
+
+[ -d "$HOME/.local/bin/rheaquery" ] && rm -rf "$HOME/.local/bin/rheaquery"
+[ -d "$HOME/.local/bin/rheaquery" ] || mkdir $HOME/.local/bin/rheaquery
+
+rm -r * $HOME/.local/bin/rheaquery/*
 cp -r src $HOME/.local/bin/rheaquery
 cp LICENSE $HOME/.local/bin/rheaquery
 
-echo 'export PATH="$HOME/.local/bin/rheaquery/src:$PATH"' >> $HOME/.bashrc
+grep 'export PATH="$HOME/.local/bin/rheaquery/src:$PATH"' $HOME/.bashrc
+|| echo 'export PATH="$HOME/.local/bin/rheaquery/src:$PATH"' >> $HOME/.bashrc
 
-echo "Rheaquery downloaded successfully. Please run `source ~/.bashrc` for full installation."
+echo "Rheaquery downloaded successfully. Please run \`source ~/.bashrc\` for full installation."
